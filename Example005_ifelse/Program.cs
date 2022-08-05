ac7253bc961d152cf4da78f3bf4f2ada1e78a1e9
PolinaLihane <polin@list.ru>
2022-08-05 13:44:47 +0300

    так то уже добавила еще 2 задачи

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите ваше имя ");
+string username1 = Console.ReadLine();
+
+if (username1.ToLower() == "m")
+{
+    Console.WriteLine("Ура!Это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username1);
+}
