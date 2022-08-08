cd36621a6e0c7e7cc10ad1112d2e105b132788a1
PolinaLihane <polin@list.ru>
2022-08-08 22:11:22 +0300

    решили все задачи 2го семинара

--- Example005_ifelse/Program.cs
+++ Example005_ifelse/Program.cs
@@ -1,12 +1,14 @@
-﻿Console.Write("введите ваше имя ");
-string username1 = Console.ReadLine();
+﻿
+Console.Write("введите ваше имя ");
+string username = Console.ReadLine();
 
-if (username1.ToLower() == "m")
+if(username.ToLower() == "маша")
 {
     Console.WriteLine("Ура!Это же Маша");
 }
 else
 {
     Console.Write("Привет, ");
-    Console.WriteLine(username1);
+    Console.WriteLine(username);
 }
+
---
+++ Example010_MethofArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+Clear();
+int[] array = {1,2,3,4,5,14,4,9};
+
+int n = array.Length;
+int find = 4;
+
+int index =0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        WriteLine(index);
+        break;
+    }
+    index++;
+}
